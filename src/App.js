db2a775752080e4df1273acd652ad96fbd8e7b33
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2022-01-27 19:56:10 +0200

    Added Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
-import Quiz from './containeers/Quiz/Quiz';
+import { Route, Routes } from "react-router-dom";
+import Quiz from "./containeers/Quiz/Quiz";
+import QuizCreator from "./containeers/QuizCreator/QuizCreator";
+import Auth from "./containeers/Auth/Auth";
+import QuizList from "./containeers/QuizList/QuizList";
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <Layout>
-        <Quiz></Quiz>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/quiz-creator" element={<QuizCreator />} />
+          <Route path="/quiz/:id" element={<Quiz />} />
+          <Route path="/" element={<QuizList />} />
+        </Routes>
       </Layout>
-    )
+    );
   }
 }
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import classes from './Layout.module.css';
 import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
-
+import Drawer from "../../components/Navigation/Drawer/Drawer";
 
 class Layout extends Component {
 
@@ -15,13 +15,24 @@ class Layout extends Component {
     })
   }
 
+  menuCloseHandler = () => {
+    this.setState({
+      menu: false
+    })
+  }
+
   render () {
     return (
       <div className={classes.Layout}>
-      <MenuToggle
-        onToggle = { this.toggleMenuHandler }
-        isOpen = { this.state.menu }
-      />
+        <Drawer 
+          isOpen = { this.state.menu }
+          onClose = { this.menuCloseHandler }
+        />
+        <MenuToggle
+          onToggle = { this.toggleMenuHandler }
+          isOpen = { this.state.menu }
+
+        />
 
         <main>
             {this.props.children}
