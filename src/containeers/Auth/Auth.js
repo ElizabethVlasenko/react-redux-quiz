b5fd8f0fd5bad21e83b27b58099c82006c06a45f
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2022-01-28 16:04:27 +0200

    Added login form

--- src/containeers/Auth/Auth.js
+++ src/containeers/Auth/Auth.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import classes from "./Auth.module.css";
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
 export default class Auth extends Component {
+  loginHandler = () => {};
+  signupHandler = () => {};
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Auth</h1>
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <Input label="Email" />
+            <Input label="Password" errorMessage="Test" />
+            <Button type="success" onClick={this.loginHandler}>
+              Login
+            </Button>
+            <Button type="primary" onClick={this.signupHandler}>
+              Signup
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
